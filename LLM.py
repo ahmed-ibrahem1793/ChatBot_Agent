3fec1a973c50693da344019a70640a3d621a6236
ahmed-ibrahem1793 <hema179369@gmail.com>
2026-09-19 16:09:52 +0300

    removed comments from LLM.py

--- LLM.py
+++ LLM.py
@@ -1,5 +1,3 @@
-# from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
-# from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage, SystemMessage, ToolMessage
 import threading
